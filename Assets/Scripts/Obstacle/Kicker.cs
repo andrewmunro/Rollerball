d66f9058fc294115e736fc4c690e2b4e84e00793
Andrew <synestry@gmail.com>
2017-01-30 22:43:56 +0000

    Initial Commit

---
+++ Assets/Scripts/Obstacle/Kicker.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Obstacle
+{
+    public class Kicker : MonoBehaviour
+    {
+        public float Power = 1000.0F;
+
+        private Vector3 direction;
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.tag == "Player")
+            {
+                var playerRigidBody = collision.gameObject.GetComponent<Rigidbody>();
+
+                direction = (collision.contacts[0].point - transform.position).normalized;
+
+                playerRigidBody.AddForce(direction * Power, ForceMode.Impulse);
+            }
+        }
+
+        private void Update()
+        {
+            if (direction != null)
+            {
+                Debug.DrawRay(transform.position, direction, Color.magenta);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class PlayerController : MonoBehaviour
+    {
+        public float Speed = 70f;
+
+        public Camera Camera;
+
+        private Rigidbody rigidbody;
+
+        private void Start()
+        {
+            rigidbody = GetComponent<Rigidbody>();
+
+            if (Camera == null)
+            {
+                Camera = Camera.main;
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            var movement = Input.GetAxis("Vertical") * Camera.transform.forward + Input.GetAxis("Horizontal") * Camera.transform.right;
+            movement.y = -1f;
+
+            rigidbody.AddForce(movement * Speed);
+            Debug.DrawRay(transform.position, rigidbody.velocity, Color.red);
+
+            Debug.DrawRay(transform.position, (rigidbody.velocity.normalized * -1) * 5, Color.green);
+        }
+    }
+}
